04e855e1ae104b0747c861de53cd9ce3851450c3
Philip Chang <sgnoohc@gmail.com>
2017-07-20 10:24:47 -0700

    update to CMSSW_9_2_4 with gcc530

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 
 # Setup ROOT
 source /code/osgcode/cmssoft/cmsset_default.sh  > /dev/null 2>&1
-export SCRAM_ARCH=slc6_amd64_gcc491
-export CMSSW_VERSION=CMSSW_7_4_1
-echo Setting up CMSSW for CMSSW_7_4_1 for slc6_amd64_gcc491
-cd /cvmfs/cms.cern.ch/slc6_amd64_gcc491/cms/cmssw/CMSSW_7_4_1/src
+export SCRAM_ARCH=slc6_amd64_gcc530   # or whatever scram_arch you need for your desired CMSSW release
+export CMSSW_VERSION=CMSSW_9_2_4
+echo Setting up CMSSW for CMSSW_9_2_4 for slc6_amd64_gcc530
+cd /cvmfs/cms.cern.ch/slc6_amd64_gcc530/cms/cmssw/CMSSW_9_2_4/src
 eval `scramv1 runtime -sh`
 cd -
 
